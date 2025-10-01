fae6fe7e25f1a3115416fb4070eeffe72d6a3bfa
Carloz2er <73202676+Carloz2er@users.noreply.github.com>
2025-09-30 22:41:45 -0400

    Delete hash.py

--- hash.py
+++
@@ -1,7 +0,0 @@
-from werkzeug.security import generate_password_hash
-
-password = "23142173"
-hashed_password = generate_password_hash(password, method='scrypt')
-
-print("Copie este hash:")
-print(hashed_password)
